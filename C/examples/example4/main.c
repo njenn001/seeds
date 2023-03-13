c2b74923a6771b2008929330d9ea9c177a061b90
njenning <njenn001@odu.edu>
2023-03-13 19:35:29 -0400

    Example update 0.4.2

--- C/examples/example4/main.c
+++ C/examples/example4/main.c
@@ -2,19 +2,38 @@
 
 // Header files for imports. 
 #include <stdio.h> 
-#include <vector>
-#include <string>
-#include <any>
-#include <algorithm>
-#include <iostream>
 
 // main function -
 // where execution begins. 
 int main(){
 
-    // Variables 
-    int n;
+    // Length.
+    int n; 
 
+    // Get list length.
+    printf("Enter a length: ");
+    scanf("%d", &n); 
+
+    // List. 
+    int nums[n]; 
+
+    // Loop to populate the new list. 
+    for (int i = 0; i < n; i ++)
+    {
+        printf("Enter a value: ");
+        scanf("%d", &nums[i]);
+    }
+
+    printf("\n"); 
+
+    // Loop to show the list elements. 
+    for (int i = 0; i < n; i ++)
+    {
+        printf("%d ", nums[i]); 
+    }
+
+    printf("\n");
+    
     //Conclusion.
     return 0; 
 }
