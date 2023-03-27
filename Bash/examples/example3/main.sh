9ecbcce8ab0d1eab92056c48bdb8e73befd91ae1
njenning <njenn001@odu.edu>
2023-03-27 17:53:19 -0400

    Documentation Standard 0.4.1

--- Bash/examples/example3/main.sh
+++ Bash/examples/example3/main.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+# Defines the main function. 
 main(){
+
+    # Declare variables.
     x=2 
     y=2
 
+    # Addition.
     sum=`expr $x + $y`
 
+    # Condition to check if number is odd or even. 
     if [ $((sum%2)) == 0 ]; then
         echo The sum is even.
     else
         echo The sum is odd.
     fi
 
+    # Print the sum.
     echo The sum is: $sum
 }
 
+# Runs the main function. 
 main
